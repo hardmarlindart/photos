45d5850c88f789141316767f3acf0d8bf5a51dfe
vagrant <vagrant@precise64.(none)>
2015-02-08 07:41:31 -0200

    Gerando  Aplicacao usando Rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photo::Application.config.secret_key_base = 'aa8942a7927439e4ed920b47f2840126e62656ddd33f2f092011fd9264dfaa8703444ca66e082cee16f1c6a6700838e4d911dae3e504a94646cca75efebefecf'
